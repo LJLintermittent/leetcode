be0c35f37218947caa0021df45adb3cbd00e0a3e
lijiale <1157221853@qq.com>
2021-06-30 20:41:31 +0800

    买卖股票的最佳时机(三)

--- src/main/java/com/learn/leetcode/onehundredToonehundredFifty/LC121.java
+++ src/main/java/com/learn/leetcode/onehundredToonehundredFifty/LC121.java
@@ -19,17 +19,21 @@ public class LC121 {
 
     /**
      * 买卖股票的最佳时机
+     * 思路：将自己买入的那天的价格想象为最低点 minprice
+     * 遍历，如果后面某天的价格小于minprice，那么改变minprice
+     * 否则代表后面某天的价格大于等于minprice，那么就用那天的价格减去minprice跟刚开始设定的
+     * 最大收益来比较，直到找到最大受益
      */
     public static int maxProfit(int[] prices) {
         int minprice = Integer.MAX_VALUE;
-        int maxprice = 0;
+        int maxprofit = 0;
         for (int i = 0; i < prices.length; i++) {
             if (prices[i] < minprice) {
                 minprice = prices[i];
-            } else if (prices[i] - minprice > maxprice) {
-                maxprice = prices[i] - minprice;
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
             }
         }
-        return maxprice;
+        return maxprofit;
     }
 }
---
+++ src/main/java/com/learn/leetcode/onehundredToonehundredFifty/LC123.java
@@ -0,0 +1,45 @@
+package com.learn.leetcode.onehundredToonehundredFifty;
+
+/**
+ * Description:
+ * date: 2021/6/30 11:42
+ * Package: com.learn.leetcode.onehundredToonehundredFifty
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC123 {
+
+    public static void main(String[] args) {
+        int[] prices = {3,3,5,0,0,3,1,4};
+        int i = maxProfit(prices);
+        System.out.println(i);
+
+    }
+
+    /**
+     * 买卖股票的最佳时机（三）
+     * 最多只能完成两次交易
+     */
+    public static int maxProfit(int[] prices) {
+        /**
+         * 任意一天考虑四个变量
+         * fstbuy：在该天第一次买入可获得的最大收益
+         * fstsell：在该天第一次卖出可获得的最大收益
+         * secbuy：在该天第二次买入可获得的最大收益
+         * secsell：在该天第二次卖出可获得的最大收益
+         */
+        int fstbuy = Integer.MIN_VALUE;
+        int fstsell = 0;
+        int secbuy = Integer.MIN_VALUE;
+        int secsell = 0;
+        for (int price : prices) {
+            fstbuy = Math.max(fstbuy, -price);
+            fstsell = Math.max(fstsell, fstbuy + price);
+            secbuy = Math.max(secbuy, fstsell - price);
+            secsell = Math.max(secsell, secbuy + price);
+        }
+        return secsell;
+    }
+}
