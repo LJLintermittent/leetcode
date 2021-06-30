2228ca4ff0b1e03e938b78b13f9d5896ec56e9a5
lijiale <1157221853@qq.com>
2021-06-30 22:20:45 +0800

    二叉树的后序遍历（迭代法）

--- src/main/java/com/learn/leetcode/onehundredToonehundredFifty/LC145.java
+++ src/main/java/com/learn/leetcode/onehundredToonehundredFifty/LC145.java
@@ -2,8 +2,7 @@ package com.learn.leetcode.onehundredToonehundredFifty;
 
 import com.learn.leetcode.utils.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Description:
@@ -17,24 +16,27 @@ import java.util.List;
 public class LC145 {
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-        TreeNode node1 = new TreeNode(2);
-        TreeNode node2 = new TreeNode(3);
-        TreeNode node3 = new TreeNode(4);
-        TreeNode node4 = new TreeNode(5);
+        TreeNode root = new TreeNode(3);
+        TreeNode node1 = new TreeNode(9);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(7);
 
         root.left = node1;
         root.right = node2;
-        node1.left = node3;
-        node2.left = node4;
+        node2.left = node3;
+        node2.right = node4;
 
-        List<Integer> list = postorderTraversal(root);
+        List<Integer> list = postorderTraversal1(root);
         System.out.println(list);
-
     }
 
     static List<Integer> list = new ArrayList<>();
 
+    /**
+     * 二叉树的后序遍历
+     * 递归
+     */
     public static List<Integer> postorderTraversal(TreeNode root) {
         if (root != null) {
             helper(root);
@@ -50,6 +52,25 @@ public class LC145 {
             helper(root.right);
         }
         list.add(root.val);
+    }
+
+    /**
+     * 迭代
+     */
+    public static List<Integer> postorderTraversal1(TreeNode root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                ans.addFirst(root.val);
+                root = root.right;
+            } else {
+                root = stack.pop();
+                root = root.left;
+            }
+        }
+        return ans;
     }
 
 }
