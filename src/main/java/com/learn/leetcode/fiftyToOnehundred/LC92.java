175a4dbf205715c1f0f2dc8a7cfa315f1f8f1a88
lijiale <1157221853@qq.com>
2021-07-01 15:35:53 +0800

    反转链表（二）

---
+++ src/main/java/com/learn/leetcode/fiftyToOnehundred/LC92.java
@@ -0,0 +1,57 @@
+package com.learn.leetcode.fiftyToOnehundred;
+
+import com.learn.leetcode.utils.ListNode;
+
+/**
+ * Description:
+ * date: 2021/7/1 15:00
+ * Package: com.learn.leetcode.fiftyToOnehundred
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC92 {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(9);
+        ListNode node1 = new ListNode(7);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(5);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(6);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        System.out.println(head);
+        ListNode ans = reverseBetween(head, 3, 6);
+        System.out.println(ans);
+    }
+
+    /**
+     * 反转链表（二）
+     */
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+
+
+}
