1ad4b17d1802014aad7c38eff2ecd82014333d55
lijiale <1157221853@qq.com>
2021-07-15 21:07:49 +0800

    从前序与中序遍历构造一棵二叉树

---
+++ src/main/java/com/learn/leetcode/onehundredToonehundredFifty/LC105.java
@@ -0,0 +1,47 @@
+package com.learn.leetcode.onehundredToonehundredFifty;
+
+import com.learn.leetcode.utils.TreeNode;
+
+import java.util.Arrays;
+
+/**
+ * Description:
+ * date: 2021/7/15 20:15
+ * Package: com.learn.leetcode.onehundredToonehundredFifty
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC105 {
+
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        TreeNode root = buildTree(preorder, inorder);
+        System.out.println(root);
+    }
+
+    /**
+     * 从前序与中序遍历序列构造二叉树
+     */
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 0; i < preorder.length; i++) {
+            if (preorder[0] == inorder[i]) {
+                //将中序数组以inorder[i]节点为界，这个节点是根节点，那么分为两半，左边为左子树，右边为右子树
+                int[] preLeft = Arrays.copyOfRange(preorder, 1, i + 1);
+                int[] preRight = Arrays.copyOfRange(preorder, i + 1, preorder.length);
+                int[] inLeft = Arrays.copyOfRange(inorder, 0, i);
+                int[] inRight = Arrays.copyOfRange(inorder, i + 1, inorder.length);
+                root.left = buildTree(preLeft, inLeft);
+                root.right = buildTree(preRight, inRight);
+                break;
+            }
+        }
+        return root;
+    }
+}
