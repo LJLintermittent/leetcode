54e250b97c633a22737ede7ee7509f169ec6fb71
lijiale <1157221853@qq.com>
2021-07-15 21:39:25 +0800

    从前序与中序遍历构造一棵二叉树（测试）

---
+++ src/test/java/com/learn/leetcode/test/LC105Test.java
@@ -0,0 +1,42 @@
+package com.learn.leetcode.test;
+
+import com.learn.leetcode.utils.TreeNode;
+
+import java.util.Arrays;
+
+/**
+ * Description:
+ * date: 2021/7/15 21:10
+ * Package: com.learn.leetcode.test
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC105Test {
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        TreeNode root = buildTree(preorder, inorder);
+        System.out.println(root);
+    }
+
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 0; i < preorder.length; i++) {
+            if (inorder[i] == preorder[0]) {
+                int[] pre_left = Arrays.copyOfRange(preorder, i, i + 1);
+                int[] pre_right = Arrays.copyOfRange(preorder, i + 1, preorder.length);
+                int[] in_left = Arrays.copyOfRange(inorder, 0, i);
+                int[] in_right = Arrays.copyOfRange(inorder, i + 1, inorder.length);
+                root.left = buildTree(pre_left, in_left);
+                root.right = buildTree(pre_right, in_right);
+                break;
+            }
+        }
+        return root;
+    }
+}
