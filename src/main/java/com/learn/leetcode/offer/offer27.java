cbce4522d3798a91584a8d7a4ff6cffd979d95e0
lijiale <1157221853@qq.com>
2021-07-16 09:23:10 +0800

    剑指offer 二叉树的镜像

---
+++ src/main/java/com/learn/leetcode/offer/offer27.java
@@ -0,0 +1,49 @@
+package com.learn.leetcode.offer;
+
+import com.learn.leetcode.utils.TreeNode;
+
+/**
+ * Description:
+ * date: 2021/7/16 9:18
+ * Package: com.learn.leetcode.offer
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class offer27 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(7);
+        TreeNode node3 = new TreeNode(1);
+        TreeNode node4 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(6);
+        TreeNode node6 = new TreeNode(9);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.left = node5;
+        node2.right = node6;
+        TreeNode ans = mirrorTree(root);
+        System.out.println(ans);
+
+    }
+
+    /**
+     * 二叉树的镜像
+     */
+    public static TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        return root;
+    }
+}
