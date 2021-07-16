52ffc2dc593d631b020bd054cc92305cd99e3445
lijiale <1157221853@qq.com>
2021-07-16 23:25:37 +0800

    数组中出现次数超过一半的数字

--- src/main/java/com/learn/leetcode/offer/offer39.java
+++ src/main/java/com/learn/leetcode/offer/offer39.java
@@ -1,37 +1,34 @@
-package com.learn.leetcode.onehundredFiftyToTwohundred;
-
+package com.learn.leetcode.offer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Description:
- * date: 2021/6/28 14:00
- * Package: com.learn.leetcode.onehundredFiftyToTwohundred
+ * date: 2021/7/16 23:12
+ * Package: com.learn.leetcode.offer
  *
  * @author 李佳乐
  * @email 18066550996@163.com
  */
 @SuppressWarnings("all")
-public class LC169 {
+public class offer39 {
 
     public static void main(String[] args) {
-        int[] arr1 = {3, 2, 3};
-        int[] arr2 = {2, 2, 1, 1, 1, 2, 2};
-        int[] arr3 = {-1, 1, 1, 1, 2, 1};
-        int i = majorityElement(arr3);
+        int[] arr = {-1, 1, 1, 1, 2, 1};
+        int i = majorityElement(arr);
         System.out.println(i);
 
     }
 
     /**
-     * 多数元素
-     * 多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素
+     * 数组中出现次数超过一半的数字
      */
     public static int majorityElement(int[] nums) {
         int length = nums.length;
         Map<Integer, Integer> map = new HashMap<>(16);
-        int count = 1;
+        int count = 0;
         for (int i = 0; i < length; i++) {
             if (map.containsKey(nums[i])) {
                 count = map.get(nums[i]) + 1;
@@ -42,11 +39,12 @@ public class LC169 {
             }
         }
         for (Integer key : map.keySet()) {
-            int o = (int) map.get(key);
-            if (o > (length / 2)) {
+            Integer value = map.get(key);
+            if (value > (length / 2)) {
                 return key;
             }
         }
         return 0;
     }
+
 }
