bc3bd3f84b0791a11698b93edfd2531c9b4e7404
lijiale <1157221853@qq.com>
2021-07-19 23:10:08 +0800

    下一个排列 测试

--- src/main/java/com/learn/leetcode/oneToFifty/LC31.java
+++ src/main/java/com/learn/leetcode/oneToFifty/LC31.java
@@ -14,7 +14,8 @@ import java.util.Arrays;
 public class LC31 {
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,8,5,7,6,4};
+        int[] nums = {1, 2, 3, 8, 5, 7, 6, 4};
+        int[] n2 = {3, 2, 1};
         nextPermutation(nums);
         System.out.println(Arrays.toString(nums));
 
@@ -26,14 +27,14 @@ public class LC31 {
     public static void nextPermutation(int[] nums) {
         int len = nums.length;
         for (int i = len - 1; i > 0; i--) {
+            //从后往前遍历，碰到的第一个升序的相邻元素对
             if (nums[i] > nums[i - 1]) {
-                // i:from index; len: to index
+                //那么这时候i的后面的元素一定是降序的
+                //我们再调成升序，以取最小
                 Arrays.sort(nums, i, len);
                 for (int j = i; j < len; j++) {
                     if (nums[j] > nums[i - 1]) {
-                        int temp = nums[j];
-                        nums[j] = nums[i - 1];
-                        nums[i - 1] = temp;
+                        swap(nums, j, i - 1);
                         return;
                     }
                 }
@@ -41,5 +42,11 @@ public class LC31 {
         }
         Arrays.sort(nums);
         return;
+    }
+
+    private static void swap(int[] nums, int m, int n) {
+        int temp = nums[m];
+        nums[m] = nums[n];
+        nums[n] = temp;
     }
 }
