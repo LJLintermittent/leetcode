ed1367799de4cb6128b90f9017ba8344fc3f0711
lijiale <1157221853@qq.com>
2021-07-19 23:54:14 +0800

    K个一组链表翻转

---
+++ src/main/java/com/learn/leetcode/oneToFifty/LC25.java
@@ -0,0 +1,50 @@
+package com.learn.leetcode.oneToFifty;
+
+import com.learn.leetcode.utils.ListNode;
+
+/**
+ * Description:
+ * date: 2021/7/19 23:12
+ * Package: com.learn.leetcode.oneToFifty
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC25 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * K个一组翻转链表
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        if (k == 1) {
+            return head;
+        }
+        int length = getLength(head);
+        if (length % k != 0) {
+            for (int i = 0; i < k; i++) {
+
+
+            }
+        }
+
+        return null;
+    }
+
+    private static int getLength(ListNode head) {
+        int length = 1;
+        while (head.next != null) {
+            length = length + 1;
+            head = head.next;
+        }
+        return length;
+    }
+
+}
