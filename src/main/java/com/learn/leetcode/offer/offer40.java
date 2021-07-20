dd1be3c3dd234653e42d1de57328a1923bbd58e7
lijiale <1157221853@qq.com>
2021-07-20 23:45:33 +0800

    最小的K个数(大根堆)

--- src/main/java/com/learn/leetcode/offer/offer40.java
+++ src/main/java/com/learn/leetcode/offer/offer40.java
@@ -1,6 +1,8 @@
 package com.learn.leetcode.offer;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Description:
@@ -14,8 +16,9 @@ import java.util.Arrays;
 public class offer40 {
 
     public static void main(String[] args) {
-        int[] arr = {3,1,2};
-        int[] ans = getLeastNumbers(arr, 2);
+        int[] arr = {3, 1, 2,4,5,6,7};
+        int[] arr2 = {0,1,2,1};
+        int[] ans = getLeastNumbers1(arr2, 1);
         System.out.println(Arrays.toString(ans));
 
     }
@@ -28,6 +31,35 @@ public class offer40 {
         Arrays.sort(arr);
         for (int i = 0; i < k; i++) {
             ans[i] = arr[i];
+        }
+        return ans;
+    }
+
+    /**
+     * 大根堆
+     */
+    public static int[] getLeastNumbers1(int[] arr, int k) {
+        int[] ans = new int[k];
+        if (k == 0) {
+            return ans;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;
+            }
+        });
+        for (int i = 0; i < k;  i++) {
+            queue.offer(arr[i]);
+        }
+        for (int i = k; i < arr.length; i++) {
+            if (queue.peek() > arr[i]) {
+                queue.poll();
+                queue.offer(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll();
         }
         return ans;
     }
