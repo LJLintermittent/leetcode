315816e143bbbd939fe5ed9f3ea796fd820a1753
lijiale <1157221853@qq.com>
2021-07-21 21:50:23 +0800

    最小路径和 动态规划

---
+++ src/main/java/com/learn/leetcode/fiftyToOnehundred/LC64.java
@@ -0,0 +1,45 @@
+package com.learn.leetcode.fiftyToOnehundred;
+
+/**
+ * Description:
+ * date: 2021/7/21 21:22
+ * Package: com.learn.leetcode.fiftyToOnehundred
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC64 {
+
+    public static void main(String[] args) {
+        int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        int ans = minPathSum(grid);
+        System.out.println(ans);
+    }
+
+    /**
+     * 最小路径和
+     * 动态规划
+     */
+    public static int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int rows = grid.length;
+        int columns = grid[0].length;
+        int dp[][] = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[rows - 1][columns - 1];
+    }
+}
