b3f0556591b84738e0661d069a37908981be5744
lijiale <1157221853@qq.com>
2021-07-21 15:19:51 +0800

    公交线路 图的最短路 BFS

---
+++ src/main/java/com/learn/leetcode/eighthundredToeighthundredFifty/LC815.java
@@ -0,0 +1,26 @@
+package com.learn.leetcode.eighthundredToeighthundredFifty;
+
+/**
+ * Description:
+ * date: 2021/7/21 14:56
+ * Package: com.learn.leetcode.eighthundredToeighthundredFifty
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC815 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 公交路线
+     */
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+
+        return 0;
+    }
+
+}
