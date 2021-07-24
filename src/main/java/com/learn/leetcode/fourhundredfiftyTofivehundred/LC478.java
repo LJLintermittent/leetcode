fed906ce501c17818bb912ee269353e350caa7ad
lijiale <1157221853@qq.com>
2021-07-24 20:34:48 +0800

    在圆内随机生成点

--- src/main/java/com/learn/leetcode/fourhundredfiftyTofivehundred/LC478.java
+++ src/main/java/com/learn/leetcode/fourhundredfiftyTofivehundred/LC478.java
@@ -17,14 +17,28 @@ public class LC478 {
 
     }
 
+    double rad, xc, yc;
+
     /**
      * 在圆内随机生成点
      */
     public LC478(double radius, double x_center, double y_center) {
+        rad = radius;
+        xc = x_center;
+        yc = y_center;
 
     }
 
     public double[] randPoint() {
-        return null;
+        double x0 = xc - rad;
+        double y0 = yc - rad;
+
+        while (true) {
+            double xg = x0 + Math.random() * rad * 2;
+            double yg = y0 + Math.random() * rad * 2;
+            if (Math.sqrt(Math.pow((xg - xc), 2) + Math.pow((yg - yc), 2)) <= rad) {
+                return new double[]{xg, yg};
+            }
+        }
     }
 }
