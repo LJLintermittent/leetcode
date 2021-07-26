fcdc684aa64792f2bd925854afa90ee53b194956
lijiale <1157221853@qq.com>
2021-07-26 10:20:11 +0800

    平衡二叉树

---
+++ src/main/java/com/learn/leetcode/onehundredToonehundredFifty/LC110.java
@@ -0,0 +1,51 @@
+package com.learn.leetcode.onehundredToonehundredFifty;
+
+import com.learn.leetcode.utils.TreeNode;
+
+/**
+ * Description:
+ * date: 2021/7/26 9:49
+ * Package: com.learn.leetcode.onehundredToonehundredFifty
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC110 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(4);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node3.left = node4;
+
+        boolean ans = isBalanced(root);
+
+        System.out.println(ans);
+    }
+
+    /**
+     * 平衡二叉树
+     */
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(helper(root.left) - helper(root.right)) <= 1
+                && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    //计算当前节点的高度
+    private static int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(helper(root.left), helper(root.right)) + 1;
+        }
+    }
+}
