bb3dcd64c02c9949ee7bdfaf4605e942bf8b414c
lijiale <1157221853@qq.com>
2021-07-27 15:23:28 +0800

    用rand7实现rand10

---
+++ src/main/java/com/learn/leetcode/fourhundredfiftyTofivehundred/LC470.java
@@ -0,0 +1,28 @@
+package com.learn.leetcode.fourhundredfiftyTofivehundred;
+
+/**
+ * Description:
+ * date: 2021/7/27 14:37
+ * Package: com.learn.leetcode.fourhundredfiftyTofivehundred
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC470 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 用 Rand7() 实现 Rand10()
+     */
+//    public int rand10() {
+//        int num = (rand(7) - 1) * 7 + rand7();
+//        while (num > 10) {
+//            num = (rand(7) - 1) * 7 + rand7();
+//        }
+//        return num;
+//    }
+}
