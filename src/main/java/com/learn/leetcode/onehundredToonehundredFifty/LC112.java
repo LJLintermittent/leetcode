74f5e8d53fecce4be9cf217c91e68c403bc4489e
lijiale <1157221853@qq.com>
2021-07-28 18:57:28 +0800

    路径总和

---
+++ src/main/java/com/learn/leetcode/onehundredToonehundredFifty/LC112.java
@@ -0,0 +1,45 @@
+package com.learn.leetcode.onehundredToonehundredFifty;
+
+import com.learn.leetcode.utils.TreeNode;
+
+/**
+ * Description:
+ * date: 2021/7/28 18:40
+ * Package: com.learn.leetcode.onehundredToonehundredFifty
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC112 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+
+        boolean ans = hasPathSum(root, 7);
+        System.out.println(ans);
+    }
+
+    /**
+     * 路径总和
+     */
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+
+    }
+}
