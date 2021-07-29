90897595ce6531d7c74e7327f364b85ba27d4971
lijiale <1157221853@qq.com>
2021-07-29 11:15:23 +0800

    旋转图像

---
+++ src/main/java/com/learn/leetcode/oneToFifty/LC48.java
@@ -0,0 +1,46 @@
+package com.learn.leetcode.oneToFifty;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Description:
+ * date: 2021/7/29 10:10
+ * Package: com.learn.leetcode.oneToFifty
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC48 {
+
+    public static void main(String[] args) {
+        int[][] mat = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(mat);
+        int row = mat.length;
+        int col = mat[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+        }
+        System.out.println(Arrays.deepToString(mat));
+    }
+
+    /**
+     * 旋转图像
+     */
+    public static void rotate(int[][] matrix) {
+        int len = matrix.length;
+        for (int i = 0; i < len / 2; i++) {
+            for (int j = 0; j < (len + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len - j - 1][i];
+                matrix[len - j - 1][i] = matrix[len - i - 1][len - j - 1];
+                matrix[len - i - 1][len - j - 1] = matrix[j][len - i - 1];
+                matrix[j][len - i - 1] = temp;
+            }
+        }
+    }
+}
