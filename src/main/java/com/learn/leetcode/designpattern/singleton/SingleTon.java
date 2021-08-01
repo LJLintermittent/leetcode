824101725eb129f7ca5fdf0ea60035061d801bef
lijiale <1157221853@qq.com>
2021-08-01 13:13:57 +0800

    获取unsafe类的工具类

---
+++ src/main/java/com/learn/leetcode/designpattern/singleton/SingleTon.java
@@ -0,0 +1,17 @@
+package com.learn.leetcode.designpattern.singleton;
+
+/**
+ * Description:
+ * date: 2021/8/1 12:45
+ * Package: com.learn.leetcode.designpattern.singleton
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class SingleTon {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/learn/leetcode/designpattern/singleton/UnsafeUtils.java
@@ -0,0 +1,36 @@
+package com.learn.leetcode.designpattern.singleton;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * Description:
+ * date: 2021/8/1 12:48
+ * Package: com.learn.leetcode.designpattern.singleton
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class UnsafeUtils {
+
+    private static Unsafe getUnsafe() {
+        try {
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            return (Unsafe) field.get(null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Unsafe unsafe = UnsafeUtils.getUnsafe();
+        System.out.println(unsafe);
+    }
+
+}
