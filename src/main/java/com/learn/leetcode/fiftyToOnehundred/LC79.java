bdfd2b5fe3645ad2ebc5833906fc7e5d34b1f55d
lijiale <1157221853@qq.com>
2021-08-06 15:16:56 +0800

    单词搜索

---
+++ src/main/java/com/learn/leetcode/fiftyToOnehundred/LC79.java
@@ -0,0 +1,77 @@
+package com.learn.leetcode.fiftyToOnehundred;
+
+/**
+ * Description:
+ * date: 2021/8/6 14:21
+ * Package: com.learn.leetcode.fiftyToOnehundred
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC79 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 单词搜索
+     * 深度优先搜索 + 状态重置
+     */
+
+    //偏移量数组
+    private static final int[][] DIRECTIONS = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    private int rows;
+    private int cols;
+    private int len;
+    private boolean[][] visited;
+    private char[] charArray;
+    private char[][] board;
+
+    public boolean exist(char[][] board, String word) {
+        rows = board.length;
+        if (rows == 0) {
+            return false;
+        }
+        cols = board[0].length;
+        visited = new boolean[rows][cols];
+        this.len = word.length();
+        this.charArray = word.toCharArray();
+        this.board = board;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int x, int y, int begin) {
+        if (begin == len - 1) {
+            return board[x][y] == charArray[begin];
+        }
+        if (board[x][y] == charArray[begin]) {
+            visited[x][y] = true;
+            //搜索上下左右
+            for (int[] direction : DIRECTIONS) {
+                int newX = x + direction[0];
+                int newY = y + direction[1];
+                if (inArea(newX, newY) && !visited[newX][newY]) {
+                    if (dfs(newX, newY, begin + 1)) {
+                        return true;
+                    }
+                }
+            }
+            //上面搜索完退出了，说明没有顺着这个点的路径找下去找不到了，那么就回溯，重置状态变量
+            visited[x][y] = false;
+        }
+        return false;
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < rows && y >= 0 && y < cols;
+    }
+}
