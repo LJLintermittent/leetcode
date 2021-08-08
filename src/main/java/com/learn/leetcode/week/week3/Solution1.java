97160c964c583e5a852966e8ed523aa736be3570
lijiale <1157221853@qq.com>
2021-08-08 12:24:05 +0800

    周赛-删除字符使字符串变好

--- src/main/java/com/learn/leetcode/week/week3/Solution1.java
+++ src/main/java/com/learn/leetcode/week/week3/Solution1.java
@@ -22,12 +22,63 @@ public class Solution1 {
 
     }
 
+    /**
+     * 删除字符使字符串变好
+     * 模拟
+     */
     public static String makeFancyString(String s) {
-        return null;
-
+        StringBuilder stringBuilder = new StringBuilder();
+        if (s.length() <= 2) {
+            return s;
+        }
+        stringBuilder.append(s.charAt(0));
+        int count = 1;
+        char k = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != k) {
+                count = 1;
+                k = s.charAt(i);
+                stringBuilder.append(s.charAt(i));
+            } else if (count < 2) {
+                stringBuilder.append(s.charAt(i));
+                count++;
+            }
+        }
+        return stringBuilder.toString();
     }
 
+    private static StringBuilder stringBuilder = new StringBuilder();
+
     public static String makeFancyString1(String s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return s;
+        }
+        helper(s, 1);
+        return stringBuilder.toString();
+    }
+
+    public static void helper(String s, int i) {
+        while (i <= s.length() - 1) {
+            StringBuilder sb = new StringBuilder();
+            while (i < s.length() - 1 && s.charAt(i) == s.charAt(i - 1)) {
+                sb.append(s.charAt(i - 1));
+                i++;
+            }
+            if (sb.length() == 0) {
+                sb.append(s.charAt(i));
+                i++;
+            } else {
+                sb.append(s.charAt(i - 1));
+            }
+            if (sb.length() <= 2) {
+                stringBuilder.append(sb);
+            } else {
+                stringBuilder.append(sb.subSequence(0, 2));
+            }
+        }
+    }
+
+    public static String makeFancyString2(String s) {
         int len = s.length();
         StringBuilder sb = new StringBuilder();
         Map<Character, Integer> map = new HashMap<>(16);
