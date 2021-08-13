cdba0227bbaf4bee802fda832012533fc2751720
lijiale <1157221853@qq.com>
2021-08-13 11:18:36 +0800

    重建二叉树

--- src/main/java/com/learn/leetcode/offer/offer07.java
+++ src/main/java/com/learn/leetcode/offer/offer07.java
@@ -2,6 +2,8 @@ package com.learn.leetcode.offer;
 
 import com.learn.leetcode.utils.TreeNode;
 
+import java.util.Arrays;
+
 /**
  * Description:
  * date: 2021/8/13 11:05
@@ -14,14 +16,33 @@ import com.learn.leetcode.utils.TreeNode;
 public class offer07 {
 
     public static void main(String[] args) {
+        int[] pre = {3, 9, 20, 15, 7};
+        int[] in = {9, 3, 15, 20, 7};
+        TreeNode ans = buildTree(pre, in);
+        System.out.println(ans);
 
     }
 
     /**
      * 剑指 Offer 07. 重建二叉树
+     * 从前序遍历与中序遍历构造二叉树
      */
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
-
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 0; i < preorder.length; i++) {
+            if (inorder[i] == preorder[0]) {
+                int[] preLeft = Arrays.copyOfRange(preorder, 1, i + 1);
+                int[] preRight = Arrays.copyOfRange(preorder, i + 1, preorder.length);
+                int[] inLeft = Arrays.copyOfRange(inorder, 0, i);
+                int[] inRight = Arrays.copyOfRange(inorder, i + 1, inorder.length);
+                root.left = buildTree(preLeft, inLeft);
+                root.right = buildTree(preRight, inRight);
+                break;
+            }
+        }
+        return root;
     }
 }
