57e44be8f3ffc41e001c4520ac3809b98b33ffeb
lijiale <1157221853@qq.com>
2021-08-18 13:40:11 +0800

    字符串解码

---
+++ src/main/java/com/learn/leetcode/threehundredTofourhundred/LC394.java
@@ -0,0 +1,48 @@
+package com.learn.leetcode.threehundredTofourhundred;
+
+import java.util.LinkedList;
+
+/**
+ * Description:
+ * date: 2021/8/18 13:17
+ * Package: com.learn.leetcode.threehundredTofourhundred
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class LC394 {
+
+    public static void main(String[] args) {
+        String s = "3[a2[c]]";
+        String ans = decodeString(s);
+
+        System.out.println(ans);
+    }
+
+    /**
+     * 字符串解码
+     */
+    public static String decodeString(String s) {
+        StringBuilder res = new StringBuilder();
+        int multi = 0;
+        LinkedList<Integer> stack_multi = new LinkedList<>();
+        LinkedList<String> stack_res = new LinkedList<>();
+        for (Character c : s.toCharArray()) {
+            if (c == '[') {
+                stack_multi.addLast(multi);
+                stack_res.addLast(res.toString());
+                multi = 0;
+                res = new StringBuilder();
+            } else if (c == ']') {
+                StringBuilder tmp = new StringBuilder();
+                int cur_multi = stack_multi.removeLast();
+                for (int i = 0; i < cur_multi; i++) tmp.append(res);
+                res = new StringBuilder(stack_res.removeLast() + tmp);
+            } else if (c >= '0' && c <= '9') multi = multi * 10 + Integer.parseInt(c + "");
+            else res.append(c);
+        }
+        return res.toString();
+    }
+
+}
