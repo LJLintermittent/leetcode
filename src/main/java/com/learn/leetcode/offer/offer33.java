8a91238f376f911c68f9b26115b4e6bb5d10a7ff
lijiale <1157221853@qq.com>
2021-08-22 15:31:50 +0800

    二叉搜索树的后序遍历序列

--- src/main/java/com/learn/leetcode/offer/offer33.java
+++ src/main/java/com/learn/leetcode/offer/offer33.java
@@ -1,5 +1,8 @@
 package com.learn.leetcode.offer;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Description:
  * date: 2021/8/21 14:07
@@ -12,14 +15,30 @@ package com.learn.leetcode.offer;
 public class offer33 {
 
     public static void main(String[] args) {
+        int[] postorder = {1, 6, 3, 2, 5};
+        int[] postorder1 = {1,3,2,6,5};
+        boolean ans = verifyPostorder(postorder1);
 
+        System.out.println(ans);
     }
 
     /**
      * 剑指 Offer 33. 二叉搜索树的后序遍历序列
+     * 单调栈
      */
-    public boolean verifyPostorder(int[] postorder) {
-
-        return false;
+    public static boolean verifyPostorder(int[] postorder) {
+        Deque<Integer> stack = new LinkedList<>();
+        int root = Integer.MAX_VALUE;
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            if (postorder[i] > root) {
+                return false;
+            }
+            while (!stack.isEmpty() && stack.peek() > postorder[i]) {
+                //数组元素小于单调栈元素了，表示往左子树走了，记录下上一个根节点
+                root = stack.pop();
+            }
+            stack.push(postorder[i]);
+        }
+        return true;
     }
 }
