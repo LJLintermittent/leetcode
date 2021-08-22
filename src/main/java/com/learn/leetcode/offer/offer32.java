95fe76e37a01d4ecd11f9c10181bc47f2b25ec13
lijiale <1157221853@qq.com>
2021-08-22 15:59:59 +0800

    从上到下打印二叉树 III

--- src/main/java/com/learn/leetcode/offer/offer32.java
+++ src/main/java/com/learn/leetcode/offer/offer32.java
@@ -2,7 +2,7 @@ package com.learn.leetcode.offer;
 
 import com.learn.leetcode.utils.TreeNode;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Description:
@@ -24,7 +24,35 @@ public class offer32 {
      * 第一行从左到右，第二行从右到左
      */
     public List<List<Integer>> levelOrder(TreeNode root) {
-
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode topNode = queue.poll();
+                list.add(topNode.val);
+                if (topNode.left != null) {
+                    queue.offer(topNode.left);
+                }
+                if (topNode.right != null) {
+                    queue.offer(topNode.right);
+                }
+            }
+            if (flag) {
+                lists.add(list);
+            } else {
+                Collections.reverse(list);
+                lists.add(list);
+            }
+            flag = !flag;
+        }
+        return lists;
     }
 
 }
